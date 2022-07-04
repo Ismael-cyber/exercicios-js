e4347955bae6d6a67d3ef6365a56bd0f297f80e4
Ismael Silva <ismaelsousasilva07@gmail.com>
2022-07-04 20:14:29 -0300

    Exercicio 6

--- lista-exercicios/exe6.js
+++ lista-exercicios/exe6.js
@@ -3,3 +3,20 @@ e tempo de aplicação. A primeira função retornará o montante da aplicação
 o regime de juros simples e a segunda retornará o valor da aplicação sob o regime de 
 juros compostos.
 */
+
+function CalcularJuroSimples(cap, i, t) {
+    const valor = cap
+    const calculo = valor * i * t
+    return console.log(calculo), console.log(valor + calculo)
+}
+
+//CalcularJuroSimples(1200, 0.02, 14)
+
+function CalcularJuroComposto(cap, i, t) {
+    const montante = (cap * (1 + i)**t) 
+    const calculoJuros = montante - cap
+    const valorJuros = calculoJuros
+    return console.log(montante.toFixed(2)), console.log(valorJuros.toFixed(2))   
+}
+
+CalcularJuroComposto(620, 0.015, 24)
